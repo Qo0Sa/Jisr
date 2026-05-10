ba2150fa7c6d7e70341cad0b4aed89db0f17a9c6
Sarah <sara.alwateed17@gmail.com>
2026-05-10 14:54:05 +0300

    Initial Commit

---
+++ Jisr/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Jisr
+//
+//  Created by Sarah on 23/11/1447 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Jisr/JisrApp.swift
@@ -0,0 +1,17 @@
+//
+//  JisrApp.swift
+//  Jisr
+//
+//  Created by Sarah on 23/11/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct JisrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
